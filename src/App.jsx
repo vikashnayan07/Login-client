9ff5f677d594e8d8c99d1e5e5de12ccd4b73d67b
Vikash Nayan <imvikashnayan@gmail.com>
2024-07-16 12:25:36 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className=" bg-slate-600">React app</h1>
+    </>
+  );
+}
+
+export default App;
